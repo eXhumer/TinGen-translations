cd3cf8711fa27c03d6e84eeb230054a876f7e9b1
eXhumer <exhumer@member.fsf.org>
2021-01-13 20:07:04 -0700

    Update translations

--- TinGen.en_US.ts
+++ TinGen.en_US.ts
@@ -4,92 +4,93 @@
 <context>
     <name>CentralWidget</name>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="47"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="55"/>
         <source>main.google_auth_group</source>
         <translation>Google User Authorization</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="55"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="63"/>
         <source>main.auth_user_btn</source>
         <translation>Authorize</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="58"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="66"/>
         <source>main.revoke_user_btn</source>
         <translation>Revoke</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="62"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="70"/>
         <source>main.folder_select_group</source>
         <translation>Google Drive Folder Scan Selection</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="66"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="74"/>
         <source>main.select_folders_btn</source>
         <translation>Select Google Drive Folders To Scan</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="70"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="78"/>
         <source>main.index_options_group</source>
         <translation>Index Options</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="76"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="84"/>
         <source>main.index_options_group.zstd_compress_btn</source>
         <translation>zstd</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="77"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="85"/>
         <source>main.index_options_group.zlib_compress_btn</source>
         <translation>zlib</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="78"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="86"/>
         <source>main.index_options_group.no_compress_btn</source>
         <translation>No Compression</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="85"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="93"/>
         <source>main.index_options_group.success</source>
         <translation>Success?</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="93"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="101"/>
         <source>main.index_options_group.title_id_filter</source>
         <translation>Title ID Filter</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="94"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="102"/>
         <source>main.index_options_group.extension_filter</source>
         <translation>NSW Extension Filter</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="102"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="110"/>
         <source>main.index_options_group.select_output_btn</source>
         <translation>Index Output Path</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="108"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="117"/>
         <source>main.encrypt_index_options_group</source>
         <translation>Encryption Options</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="113"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="122"/>
         <source>main.encrypt_index_options_group.encrypt_index</source>
         <translation>Encrypt Index?</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="119"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="128"/>
         <source>main.encrypt_index_options_group.select_key_btn</source>
         <translation>Public Key</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="124"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="135"/>
         <source>main.generate_btn</source>
         <translation>Generate</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/centralwidget.cpp" line="144"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="164"/>
+        <location filename="../AppWidgets/centralwidget.cpp" line="175"/>
         <source>main.logged_in_as</source>
         <translation type="unfinished"></translation>
     </message>
@@ -110,39 +111,39 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../AppWidgets/mainwindow.cpp" line="35"/>
+        <location filename="../AppWidgets/mainwindow.cpp" line="37"/>
         <source>header.file</source>
         <translation>File</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/mainwindow.cpp" line="36"/>
+        <location filename="../AppWidgets/mainwindow.cpp" line="38"/>
         <source>header.file.exit</source>
         <translation>Exit</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/mainwindow.cpp" line="37"/>
+        <location filename="../AppWidgets/mainwindow.cpp" line="39"/>
         <source>header.help</source>
         <translation>Help</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/mainwindow.cpp" line="38"/>
-        <location filename="../AppWidgets/mainwindow.cpp" line="56"/>
+        <location filename="../AppWidgets/mainwindow.cpp" line="40"/>
+        <location filename="../AppWidgets/mainwindow.cpp" line="58"/>
         <source>header.help.about</source>
         <translation>About</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/mainwindow.cpp" line="39"/>
-        <location filename="../AppWidgets/mainwindow.cpp" line="61"/>
+        <location filename="../AppWidgets/mainwindow.cpp" line="41"/>
+        <location filename="../AppWidgets/mainwindow.cpp" line="63"/>
         <source>header.help.aboutqt</source>
         <translation>About Qt</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/mainwindow.cpp" line="48"/>
+        <location filename="../AppWidgets/mainwindow.cpp" line="50"/>
         <source>status.ready</source>
         <translation>Ready!</translation>
     </message>
     <message>
-        <location filename="../AppWidgets/mainwindow.cpp" line="56"/>
+        <location filename="../AppWidgets/mainwindow.cpp" line="58"/>
         <source>header.help.about.text</source>
         <translation type="unfinished"></translation>
     </message>
